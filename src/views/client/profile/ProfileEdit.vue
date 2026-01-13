4299b1887680f5569c309aa9eac50ec06937e205
jc-1001 <macio6898@gmail.com>
2026-01-13 12:57:15 +0800

    清除後台檔案、新增個人中心(製作中)

---
+++ src/views/client/profile/ProfileEdit.vue
@@ -0,0 +1,8 @@
+<script setup>
+import { ref } from 'vue'
+import TheProfileHeader from '@/components/common/TheProfileHeader.vue'
+</script>
+<template>
+  <TheProfileHeader />
+</template>
+<style lang="scss" scoped></style>
